ac19042d516b44026d6f6e661853634a71df07d6
croberts808 <crobert7@email.unc.edu>
2023-04-23 14:48:18 -0400

    add error messages

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,2 +1,38 @@
 #!/usr/bin/env node
-import {rps} from "/lib/rpsls.js"
+import { rps } from 'node-rpsls'
+import minimist from 'minimist'
+
+const args = minimist(process.argv.slice(2));
+
+if (args.h || args.help) {
+    
+    console.log('Usage: node-rps [SHOT]\nPlay Rock Paper Scissors (RPS)\n\n',
+    '  -h, --help      display this help message and exit\n',
+    '  -r, --rules     display the rules and exit\n\nExamples:\n',
+    '  node-rps        Return JSON with single player RPS result.\n',
+    '                  e.g. {"player":"rock"}\n',
+    '  node-rps rock   Return JSON with results for RPS played against a simulated opponent.\n',
+    '                  e.g {"player":"rock","opponent":"scissors","result":"win"}')
+    process.exit(0);
+}
+
+
+if (args.r || args.rules) {
+    console.log('Rules for Rock Paper Scissors:\n\n',
+    ' - Scissors CUTS Paper\n',
+    ' - Paper COVERS Rock\n',
+    ' - Rock CRUSHES Scissors')
+    process.exit(0);
+}
+
+
+const move = args._[0]
+
+try {
+    game = rps(move)
+    console.log(JSON.stringify(rps(game)));
+} catch (e){
+    process.exit(0)
+}
+
+
