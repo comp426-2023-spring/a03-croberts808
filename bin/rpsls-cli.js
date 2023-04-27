82c2e72a5cdb8575658528689b0512f38556c82d
croberts808 <crobert7@email.unc.edu>
2023-04-27 11:17:37 -0400

    Added rules and help message for invalid argument

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -40,6 +40,17 @@ try {
     const game = rpsls(args._[0])
     console.log(JSON.stringify(game));
 } catch (e){
+    console.log('Usage: node-rps [SHOT]\nPlay Rock Paper Scissors (RPS)\n\n',
+    '  -h, --help      display this help message and exit\n',
+    '  -r, --rules     display the rules and exit\n\nExamples:\n',
+    '  node-rps        Return JSON with single player RPS result.\n',
+    '                  e.g. {"player":"rock"}\n',
+    '  node-rps rock   Return JSON with results for RPS played against a simulated opponent.\n',
+    '                  e.g {"player":"rock","opponent":"scissors","result":"win"}')
+    console.log('Rules for Rock Paper Scissors:\n\n',
+    ' - Scissors CUTS Paper\n',
+    ' - Paper COVERS Rock\n',
+    ' - Rock CRUSHES Scissors')
     process.exit(0)
 }
 
